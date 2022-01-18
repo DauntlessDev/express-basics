302263e8218c78c208008eb2642cb3ae2c6e592a
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-01-18 23:08:16 +0800

    rendered html with ejs

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.set("view engine", "ejs")
+
+
 app.get('/', (req, res) => {
     console.log('Here') 
     // res.status(500).send({message:"Hi"})
-    res.download('server.js')
+    // res.download('server.js')
+
+    res.render('index', { text: "World"})
 
 })
 
