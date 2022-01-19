64d3c0b755220ba3948e28bc72977a6b37d5d8fb
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-01-19 19:43:12 +0800

    added express routing

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  console.log("Here");
+  // res.status(500).send({message:"Hi"})
+  // res.download('server.js')
 
-app.get('/', (req, res) => {
-    console.log('Here') 
-    // res.status(500).send({message:"Hi"})
-    // res.download('server.js')
+  res.render("index", { text: "World" });
+});
 
-    res.render('index', { text: "World"})
+const userRouter = require('./routes/users')
+// const postRouter = require('./routes/posts')
 
-})
+app.use('/users', userRouter)
+// app.use('/posts', postRouter)
 
-app.listen(3000)
+app.listen(3000);
