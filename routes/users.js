6709b47700ff3ece60d37836f95911520ef74fff
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-01-20 20:45:36 +0800

    added advanced routing and params

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("User List");
@@ -9,4 +9,41 @@ router.get("/new", (req, res) => {
   res.send("User New Form");
 });
 
-module.exports = router
+router.post("/", (req, res) => {
+  res.send("Create User");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user);
+    res.send(`Get User with ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update User with ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete User with ID ${req.params.id}`);
+  });
+
+// INSTEAD OF THIS USE THE CODE ABOVE FOR CLEANER CODE
+// router.get("/:id", (req, res) => {
+//   res.send(`Get User with ID ${req.params.id}`);
+// });
+
+// router.put("/:id", (req, res) => {
+//   res.send(`Update User with ID ${req.params.id}`);
+// });
+
+// router.delete("/:id", (req, res) => {
+//   res.send(`Delete User with ID ${req.params.id}`);
+// });
+const users = [{ name: "Kyle" }, { name: "Sally" }];
+
+router.param("id", (req, res, next, id) => {
+  
+  req.user = users[id]
+  next();
+});
+
+module.exports = router;
