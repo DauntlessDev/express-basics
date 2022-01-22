a6ead7c27769394bea9bd37e0d06c39d7315e7bd
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-01-22 16:20:35 +0800

    updated to render static html

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 
+
+app.use(express.static("public"))
 app.set("view engine", "ejs");
 // app.use(logger)
 
-app.get("/", (req, res) => {
-  console.log("Here");
-  // res.status(500).send({message:"Hi"})
-  // res.download('server.js')
+// app.get("/", (req, res) => {
+//   console.log("Here");
+//   // res.status(500).send({message:"Hi"})
+//   // res.download('server.js')
 
-  res.render("index", { text: "World" });
-});
+//   res.render("index", { text: "World" });
+// });
+
+
 
 const userRouter = require('./routes/users')
 // const postRouter = require('./routes/posts')
