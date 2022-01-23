b5eecc59893ecdc3f2051e4f8a292f9280550d71
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-01-23 10:53:17 +0800

    added code for pasing form and jason

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,27 @@
+const e = require("express");
 const express = require("express");
 const router = express.Router();
 
-router.use(logger)
+router.use(logger);
 
 router.get("/", (req, res) => {
   res.send("User List");
 });
 
 router.get("/new", (req, res) => {
-  res.send("User New Form");
+  // res.send("User New Form");
+  res.render("users/new");
 });
 
 router.post("/", (req, res) => {
-  res.send("Create User");
+  const isValid = true;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("Error");
+    res.render("users/new", { firstName: req.body.firstName });
+  }
 });
 
 router
@@ -43,16 +52,13 @@ router
 const users = [{ name: "Kyle" }, { name: "Sally" }];
 
 router.param("id", (req, res, next, id) => {
-  
-  req.user = users[id]
+  req.user = users[id];
   next();
 });
 
-
-function logger(req, res, next){
-  console.log(req.originalUrl)
-  next()
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
 }
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-app.use(express.static("public"))
 app.set("view engine", "ejs");
 // app.use(logger)
 
@@ -14,12 +16,10 @@ app.set("view engine", "ejs");
 //   res.render("index", { text: "World" });
 // });
 
-
-
-const userRouter = require('./routes/users')
+const userRouter = require("./routes/users");
 // const postRouter = require('./routes/posts')
 
-app.use('/users', userRouter)
+app.use("/users", userRouter);
 // app.use('/posts', postRouter)
 
 app.listen(3000);
